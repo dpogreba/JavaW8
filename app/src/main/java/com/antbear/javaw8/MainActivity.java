71205a4fe82432472f56161d2df746db7c35c5b7
dpogreba <derek.pogrebamoore@carvana.com>
2025-06-25 14:48:33 -0700

    Initial commit: Create JavaW8 Android app with bottom navigation

---
+++ app/src/main/java/com/antbear/javaw8/MainActivity.java
@@ -0,0 +1,59 @@
+package com.antbear.javaw8;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private Fragment homeFragment, dashboardFragment, notificationsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize fragments
+        homeFragment = new HomeFragment();
+        dashboardFragment = new DashboardFragment();
+        notificationsFragment = new NotificationsFragment();
+
+        // Set the default fragment
+        loadFragment(homeFragment);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                selectedFragment = homeFragment;
+            } else if (itemId == R.id.nav_dashboard) {
+                selectedFragment = dashboardFragment;
+            } else if (itemId == R.id.nav_notifications) {
+                selectedFragment = notificationsFragment;
+            }
+
+            if (selectedFragment != null) {
+                loadFragment(selectedFragment);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    /**
+     * Replaces the current fragment with a new one.
+     * @param fragment The fragment to display.
+     */
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+}
