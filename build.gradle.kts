0a219cc04b9d9bf449f51980dd291a3e4a0b6ffc
dpogreba <derek.pogrebamoore@carvana.com>
2025-06-27 08:57:38 -0700

    chore: Update Gradle and Java compatibility
    - Updated Java compatibility from Java 8 to Java 11
    - Updated Gradle plugin from 8.2.0 to 8.3.0
    - Updated Gradle wrapper from 8.4 to 8.6
    - This should fix the deprecation warnings during build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@
 buildscript {
     // NOTE: repositories are now defined in settings.gradle.kts
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.android.tools.build:gradle:8.3.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 
